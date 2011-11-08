e571aac1bfdd6d5292e4983f5d168215f265c197
Scott Messner <blackstrype@gmail.com>
2011-11-08 16:05:29 +0100

    Corrections to float_pack.sv for successful simulation.

--- src/float_pack.sv
+++ src/float_pack.sv
@@ -1,89 +1,118 @@
 package float_pack; 
 // Ajouter ici les définition des fonctions
 // utilisée par votre coprocesseur
-  parameter N_mantisse = 20; // entre 1 et 23
-  parameter N_exposant = 4; // entre 2 et 8
+  parameter N_mantisse = `TB_MANT_SIZE; // entre 1 et 23
+  parameter N_exposant = `TB_EXP_SIZE; // entre 2 et 8
 
+  // struct for float
   typedef struct packed {
      bit                  signe;
      bit [0:N_exposant-1] exposant;
      bit [0:N_mantisse-1] mantisse;
   } float;
 
+  // struct for float_ieee
   typedef struct packed {
-     bit 		   signe; // signe
-     bit [0:7] 	   exposant; // exposant
-     bit [0:22] 	   mantisse; // mantisse 
+     bit           signe; // signe
+     bit [0:7]     exposant; // exposant
+     bit [0:22]    mantisse; // mantisse 
   } float_ieee;
 
+  /**
+   * Convert shortreal to float_ieee
+  **/
   function float_ieee real2float_ieee(shortreal s);
     real2float_ieee = $shortrealtobits(s);
   endfunction // real2float_ieee
 
-   
+  /**
+   * Convert float_ieee to shortreal
+  **/
   function shortreal float_ieee2real(float_ieee f);
     float_ieee2real = $bitstoshortreal(f);
   endfunction // float_ieee2real
-
    
+  /**
+   * Convert shortreal to float
+  **/
   function float real2float(shortreal s);
-      float_ieee tmp;
-      float tmp2;
-      tmp=real2float_ieee(s);
+    float_ieee tmp;
+    int tmp_exp;
 
-      tmp2.signe = tmp.signe;
-     if( tmp.exposant > 127+(2**(N_exposant-1)-1)  ) // e > (2^N_exposant-1 - 1)
-	begin
-	   tmp2.exposant=2**N_exposant - 2;
-	   tmp2.mantisse = 2**N_mantisse-1; // ~0
-	end
-     else
-       begin
-	if( tmp.exposant < 127+(-1)*2**(N_exposant-1)+2 )
-	begin
-	   tmp2.exposant = 0;
-	   tmp2.mantisse = 0;
-	end
-       else
-	begin
-	   tmp2.exposant = tmp.exposant - 127 + 2**(N_exposant-1) - 1; // r2f.exp = tmp.exp - 2^7 - 1
-	   tmp2.mantisse = tmp.mantisse >> (23-N_mantisse);
-	 
-	end // else: !if( (tmp.exposant - 127) < (-1)*2**(N_exposant-1)+2 )
-       end // else: !if( (tmp.exposant - 127) > (2**(N_exposant-1)-1)  )
-      
-      real2float=tmp2;
-      
-   endfunction // real2float
+    tmp=real2float_ieee(s);
+    real2float.signe = tmp.signe;
+    tmp_exp = tmp.exposant;
 
+    // if e > (2^N_exposant-1 - 1)
+    if( tmp_exp > 127+(2**(N_exposant-1)-1)  ) begin 
+        real2float.exposant=2**N_exposant - 2;
+        real2float.mantisse = 2**N_mantisse-1; // ~0
+    end else begin
+      if( tmp_exp < 127+(-1)*2**(N_exposant-1)+2 ) begin
+	    real2float.exposant = 0;
+	    real2float.mantisse = 0;
+      end else begin
+        // r2f.exp = tmp.exp - 2^7 - 1
+        real2float.exposant = tmp.exposant - 127 + 2**(N_exposant-1) - 1;
+        real2float.mantisse = tmp.mantisse >> (23-N_mantisse);
+      end // else: !if( (tmp.exposant - 127) < (-1)*2**(N_exposant-1)+2 )
+    end // else: !if( (tmp.exposant - 127) > (2**(N_exposant-1)-1)  )
 
-   function shortreal float2real(float s);
-      float_ieee tmp;
-      tmp.signe = s.signe;
-      tmp.exposant = s.exposant-(2**(N_exposant-1)-1)+127;
-      tmp.mantisse = s.mantisse << (23-N_mantisse);
-      
-      float2real = real2float_ieee(tmp);
-   endfunction // float2real
+  endfunction // real2float
 
-   // multiplication de op1 et op2
-   function float float_mult(float op1, float op2);
-      // stockage temporaire pour les resultats du multiplication
-      bit [0:N_exposant-1] exp;
-      bit [0:(N_mantisse-1) * 2] mant;
+  /**
+   * Convert float_ieee to shortreal
+  **/
+  function shortreal float2real(float s);
+    float_ieee tmp;
+    tmp.signe = s.signe;
+    tmp.exposant = s.exposant-(2**(N_exposant-1)-1)+127;
+    tmp.mantisse = s.mantisse << (23-N_mantisse);
+     
+    float2real = float_ieee2real(tmp);
+  endfunction // float2real
 
-      float_mult.signe = op1.signe ^ op2.signe; // XOR
-      float_mult.exposant = 0;
-      float_mult.mantisse = 0;
-   endfunction
+  /**
+   * multiplication de op1 et op2
+  **/
+  function float float_mult(float op1, float op2);
+    // stockage temporaire pour les resultats du multiplication
+    bit [0:N_exposant-1] exp;
+    bit [0:(N_mantisse-1) * 2] mant; // Ca puet-être ne marchera pas...
+                                     // Nous avons besoin d'un champs de 
+                                     // bits assez grands de garder le
+                                     // resultat de le multiplication du
+                                     // mantisse
 
-   function float_ieee float_addsub(float_ieee op1, float_ieee op2, bit sub);
-      // addition ou soustraction de op1 de op2
-      // if (sub), faire un soustraction
-   endfunction
+    // 1.x * 1.y = (1 + 0.x) + (1 + 0.y) 
+    //   = 1 + 0.x + 0.y + 0.x*0.y = 1.(0.x + 0.y + 0.x*0.y) = 1.(mant)
+    // mant = (0.x + 0.y + 0.x*0.y)
+    mant = op1.mantisse + op2.mantisse + op1.mantisse*op2.mantisse;
+    // if mant > 1
+    //    mant = mant >> 1;
+    //    exposant += 1;
+    // else
+    //    
+
+    float_mult.signe = op1.signe ^ op2.signe; // XOR
+    float_mult.exposant = 0;
+    float_mult.mantisse = 0;
+  endfunction
+
+  /**
+   * addition ou soustraction de op1 par op2
+  **/
+  function float_ieee float_addsub(float_ieee op1, float_ieee op2, bit sub);
+    // addition ou soustraction de op1 de op2
+    // if (sub), faire un soustraction
+    // else, faire une addition
+
+    // TODO: Write addition function
+
+    float_addsub.signe = 0;
+    float_addsub.exposant = 0;
+    float_addsub.mantisse = 0;
+  endfunction
 
 endpackage : float_pack
 
-
-   
-   
