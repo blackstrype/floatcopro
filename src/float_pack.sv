618addb4f118fc16bfc0946be981dbf0246f54ef
Scott Messner <blackstrype@gmail.com>
2011-11-16 22:42:37 +0100

    Addition and subtraction from taha

--- src/float_pack.sv
+++ src/float_pack.sv
@@ -133,20 +133,201 @@ package float_pack;
     float_mul.signe = op1.signe ^ op2.signe; // XOR
   endfunction
 
-  /**
-   * addition ou soustraction de op1 par op2
-  **/
-  function float float_addsub(float_ieee op1, float_ieee op2, bit sub);
-    // addition ou soustraction de op1 de op2
-    // if (sub), faire un soustraction
-    // else, faire une addition
+   function float float_sub(float s1,float s2);	
+//A rajouter le test sur le signe dans float_sub!!!!!!!
+   if(s1.signe==s2.signe)
+     if(s1.signe==0)
+     float_sub=float_add_sub(s1,s2,1);
+     else
+	begin
+     float_sub=float_add_sub(s2,s1,1);
+     float_sub.signe=~float_sub.signe;	
+	end
+   else
+	if({s1.exposant,s1.mantisse}!=0)	
+     float_sub=float_add_sub(s1,s2,0);
+     else
+	float_sub={~s2.signe,s2.exposant,s2.mantisse};
+   endfunction // float_add_sub
+   
+   /**
+    * addition ou soustraction de op1 par op2
+   **/
+   function float float_add_sub(float s1,float s2,bit opchoice);     
+      int delta=0;
+      int mantisse_somme=0;
+      int un_position =N_mantisse+1;
+      if(opchoice==0)           
+	begin
+	//signe du résultat
+	   float_add_sub.signe=s1.signe; //a chauqe fois que float_add est appelée dans float_add_sub le signe du résultat est toujours le signe de s1
+	   //addition
+	   if({s1.exposant,s1.mantisse}==0)
+	     float_add_sub=s2; 
+	     	     
+	   else
+	     if({s2.exposant,s2.mantisse}==0)
+	       float_add_sub=s1;
+	     else
+	       begin
+		  if(s1.exposant>=s2.exposant)
+		    begin
+		       delta=s1.exposant-s2.exposant;//delta est la valeur absolue de la difference des exposants
+       		       //addition des mantisses allignées
+		       mantisse_somme=(s1.mantisse+2**N_mantisse);		   
+		       mantisse_somme=mantisse_somme+((2**N_mantisse+s2.mantisse)>>delta);
+		    end // if (s1.exposant>=s2.exposant)
+		  else
+		    begin
+		       delta=s2.exposant-s1.exposant;
+		       //addition des mantisses allignées
+		       mantisse_somme=(s2.mantisse+2**N_mantisse);
+		       mantisse_somme=mantisse_somme+((2**N_mantisse+s1.mantisse)>>delta);
+		    end // else: !if(s1.exposant>=s2.exposant)
+		  
+		  //position du premier 1 dans la somme et génération de l'exposant  et génération de l'exposant
+		  if(mantisse_somme[N_mantisse+1]==1)
+      		    begin
+		       if(s1.exposant>=s2.exposant)
+			 begin
+			    if(s1.exposant==2**N_exposant-2)
+			      begin
+				 float_add_sub.exposant=2**N_exposant-2;
+				 float_add_sub.mantisse=2**N_mantisse-1;
+			      end
+			    else
+			      begin
+				 float_add_sub.exposant=s1.exposant+1;
+				 un_position=N_mantisse+1;
+				 mantisse_somme[un_position]=0;
+				 float_add_sub.mantisse=mantisse_somme>>1;
+			      end // else: !if(s1.exposant==2**N_exposant-1)
+			 end // if (s1.exposant>=s2.exposant)
+		       else
+			 begin
+			    if(s2.exposant==2**N_exposant-2)
+			      begin
+				 float_add_sub.exposant=2**N_exposant-2;
+				 float_add_sub.mantisse=2**N_mantisse-1;
+			      end
+			    else
+			      begin
+				 float_add_sub.exposant=s2.exposant+1;
+				 un_position=N_mantisse+1;
+				 mantisse_somme[un_position]=0;
+				 float_add_sub.mantisse=mantisse_somme>>1;
+			      end 
+			 end 		  
+		    end
+		  else 
+		    begin
+		       float_add_sub.exposant=(s1.exposant>=s2.exposant)?s1.exposant:s2.exposant;
+		       un_position=N_mantisse;
+		       mantisse_somme[un_position]=0;
+		       float_add_sub.mantisse=mantisse_somme ;
+		    end
+	       end // else: !if(s2==0)
+	end        
+else
+  begin
 
-    // TODO: Write addition function
+     if(s1==s2)
+       float_add_sub=0;
+     else
+       begin
+	  if({s2.exposant,s2.mantisse}==0)
+	    float_add_sub=s1;
+	  else
+	    if({s1.exposant,s1.mantisse}==0)
+	      begin
+		 float_add_sub=s2;
+		
+	      end
+	    else
+	      begin
+		 
+		 if(s1.exposant>s2.exposant)
+		   begin
+		      float_add_sub.signe=s1.signe;//le signe est celui du float ayant le plus grand exposant
+		      delta=s1.exposant-s2.exposant;//delta est la valeur absolue de la difference des exposant
+		      mantisse_somme=(s1.mantisse+2**N_mantisse);		   
+		      mantisse_somme=mantisse_somme-((2**N_mantisse+s2.mantisse)>>delta);
+		   end
+		 
+		 else
+		   if(s2.exposant>s1.exposant)
+		     begin
+			float_add_sub.signe= ~s1.signe;
+			delta=s2.exposant-s1.exposant;
+			mantisse_somme=(s2.mantisse+2**N_mantisse);
+			mantisse_somme=mantisse_somme-((2**N_mantisse+s1.mantisse)>>delta);
+		     end // else: !if(s1.exposant>=s2.exposant)
+		   else
+		     begin
+			if(s1.mantisse>s2.mantisse)
+			  begin
+			     float_add_sub.signe=0;
+			     mantisse_somme={0,s1.mantisse-s2.mantisse};
+			  end
+			else
+			  if(s2.mantisse>s1.mantisse)
+			    begin
+			       float_add_sub.signe=1;
+			       mantisse_somme={0,s2.mantisse-s1.mantisse};
+			    end
+			  else
+			    float_add_sub=0;
+		     end
+		 
+		 
+		 //recherche du premier un dans la mantisse
+		 un_position=N_mantisse;
+		 while(mantisse_somme[un_position] !=1 && un_position >= 0 )
+		   begin
+		      un_position=un_position-1;
+		   end
+		 
+		 if(s1.exposant>s2.exposant)
+		   if(s1.exposant>=(N_mantisse-un_position))
+		     float_add_sub.exposant=s1.exposant-(N_mantisse-un_position);
+		   else
+		     begin
+			float_add_sub.exposant=0;
+			mantisse_somme=0;
+		     end
+       		 else
+		   if(s2.exposant>s1.exposant)
+		     if(s2.exposant>=(N_mantisse-un_position))
+		       float_add_sub.exposant=s2.exposant-(N_mantisse-un_position);
+		     else
+		       begin
+			  float_add_sub.exposant=0;
+			  mantisse_somme=0;
+		       end
+		   else
+		     if(s1.mantisse!=s2.mantisse)
+		       if(s1.exposant>=(N_mantisse-un_position))
+	      		 float_add_sub.exposant=s1.exposant-(N_mantisse-un_position);
+		       else
+			 begin
+			    float_add_sub.exposant=0;
+			    mantisse_somme=0;
+			 end
+		 
+		 //génération de la nouvelle mantisse avec troncature
+		 mantisse_somme[un_position]=0;
+		 mantisse_somme=mantisse_somme<<(N_mantisse-un_position);
+		 float_add_sub.mantisse=mantisse_somme;
+		 
+	      end
+       end 
+  end      
 
-    float_addsub.signe = 0;
-    float_addsub.exposant = 0;
-    float_addsub.mantisse = 0;
-  endfunction
+     endfunction
+   
+   function float float_div(float s1,float s2);
+      
+     endfunction // float_div
 
 endpackage : float_pack
 
